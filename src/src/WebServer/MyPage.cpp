fc64c1540dd9563995b4b44c18dfb6ddad4da697
AlSeldom <57368999+AlSeldom@users.noreply.github.com>
2025-11-17 01:47:47 +0300

    dobavili button no krivo

--- src/src/WebServer/MyPage.cpp
+++ src/src/WebServer/MyPage.cpp
@@ -10,14 +10,35 @@
 #include "../Globals/ESPEasyWiFiEvent.h"
 #include "../Globals/ESPEasy_time.h"
 
+// void handle_mypage() {
+//   if (!isLoggedIn()) { return; }   // проверка авторизации
+
+//   TXBuffer.startStream();          // начать формирование HTML
+//   sendHeadandTail_stdtemplate(_HEAD);
+
+//   addFormHeader(F("My Custom Page"));
+//   addHtml(F("<p>Hello from MyPage!</p>"));
+
+//   sendHeadandTail_stdtemplate(_TAIL);
+// }
+
+// #endif // WEBSERVER_MY
+
 void handle_mypage() {
   if (!isLoggedIn()) { return; }   // проверка авторизации
 
-  TXBuffer.startStream();          // начать формирование HTML
+  TXBuffer.startStream();
   sendHeadandTail_stdtemplate(_HEAD);
 
   addFormHeader(F("My Custom Page"));
+
+  // --- Приветствие ---
   addHtml(F("<p>Hello from MyPage!</p>"));
+
+  // --- Управление реле ---
+  addFormSubHeader(F("Relay Control"));
+  addButton(F("/?cmd=GPIO,4,1&page=mypage"), F("Relay 1 ON"));
+  addButton(F("/?cmd=GPIO,4,0&page=mypage"), F("Relay 1 OFF"));
 
   sendHeadandTail_stdtemplate(_TAIL);
 }
