87e8724d4646b414520f0f2b47d3b5329f1977aa
AlSeldom <57368999+AlSeldom@users.noreply.github.com>
2025-11-17 01:19:45 +0300

    add standart webpage - mypage

--- src/src/WebServer/MyPage.cpp
+++ src/src/WebServer/MyPage.cpp
@@ -1,10 +1,25 @@
 #include "../WebServer/MyPage.h"
 
 #ifdef WEBSERVER_MY
+
 #include "../WebServer/ESPEasy_WebServer.h"
+#include "../WebServer/HTML_wrappers.h"
+#include "../WebServer/Markup.h"
+#include "../WebServer/Markup_Buttons.h"
+#include "../WebServer/Markup_Forms.h"
+#include "../Globals/ESPEasyWiFiEvent.h"
+#include "../Globals/ESPEasy_time.h"
 
 void handle_mypage() {
-  // Минимальный тестовый ответ
-  web_server.send(200, F("text/plain"), F("Hello from MyPage"));
+  if (!isLoggedIn()) { return; }   // проверка авторизации
+
+  TXBuffer.startStream();          // начать формирование HTML
+  sendHeadandTail_stdtemplate(_HEAD);
+
+  addFormHeader(F("My Custom Page"));
+  addHtml(F("<p>Hello from MyPage!</p>"));
+
+  sendHeadandTail_stdtemplate(_TAIL);
 }
-#endif
+
+#endif // WEBSERVER_MY
